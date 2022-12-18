1c933946b04742cdd361b71100052d6a247ae2f4
Bastian Somon <bastian.somon@gmail.com>
2022-12-18 03:48:07 +0100

    Edit pwa

--- mp-ui/vite.config.ts
+++ mp-ui/vite.config.ts
@@ -2,10 +2,28 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Vite PWA",
+        short_name: "Vite PWA",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "/favicon.ico",
+            sizes: "32x32",
+            type: "image/x-icon",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
